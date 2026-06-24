cb1816190d41c8121d5776b055a73853e59ebdda
zhijunio <zhijun.lab@gmail.com>
2026-06-24 20:49:34 +0800

    docs: web 模块 package-info 补充占位说明

--- rose-spring-boot/rose-spring-boot-web/src/main/java/io/zhijun/boot/web/package-info.java
+++ rose-spring-boot/rose-spring-boot-web/src/main/java/io/zhijun/boot/web/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Rose Spring Boot Web auto-configuration for {@link io.zhijun.spring.web}.
+ * <p>
+ * This module is currently a placeholder. Implementation is planned per the design spec.
  *
  * @see <a href="https://github.com/zhijun-io/rose/blob/main/docs/rose-spring-web-handler-design.md">design spec</a>
  */
--- rose-spring/rose-spring-web/src/main/java/io/zhijun/spring/web/package-info.java
+++ rose-spring/rose-spring-web/src/main/java/io/zhijun/spring/web/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Rose Spring Web: MVC endpoint registry and {@code HandlerMethod} interception SPI.
+ * <p>
+ * This module is currently a placeholder. Implementation is planned per the design spec.
  *
  * @see <a href="https://github.com/zhijun-io/rose/blob/main/docs/rose-spring-web-handler-design.md">design spec</a>
  */
